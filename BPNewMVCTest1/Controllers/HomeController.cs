4620d0850bc1f67d4a21559fd7284e92f498c3b0
Bhathiya Dasanayake <bhathiyamadusanka@gmail.com>
2018-08-08 10:59:55 +0530

    Change in contact page

--- BPNewMVCTest1/Controllers/HomeController.cs
+++ BPNewMVCTest1/Controllers/HomeController.cs
@@ -43,7 +43,7 @@ namespace BPNewMVCTest1.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page here 12345.";
+            ViewData["Message"] = "Your contact page here.";
 
             return View();
         }
